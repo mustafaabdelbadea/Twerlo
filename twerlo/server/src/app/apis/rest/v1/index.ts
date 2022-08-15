489ae476600ccaeb5a966de3963e41a69ff1fdc2
mustafaabdelbadea <mustafa.abdelbadea@gmail.com>
2022-08-15 09:46:42 +0200

    Add words endpoint

--- twerlo/server/src/app/apis/rest/v1/index.ts
+++ twerlo/server/src/app/apis/rest/v1/index.ts
@@ -5,11 +5,21 @@ import TestController from '../../../controllers/test.controller';
 const router = Router();
 
 
+router.get('/words', (req, res) => {
+    try {
+        const data = TestController.getWords();
+        res.send(data).status(200)
+    } catch (error) {
+    console.log("🚀 ~ file: index.ts ~ line 12 ~ router.get ~ error", error)
+    }
+})
+
+
 router.post('/rank', (req, res) => {
     try {
         if(req.body.score != undefined || req.body.score != null) {
          const  data = TestController.calculateRank(req.body.score);
-           res.send(data)
+           res.send(data).status(200)
         } else {
             res.status(400).send("No score provided")
         }
--- twerlo/server/src/app/controllers/test.controller.ts
+++ twerlo/server/src/app/controllers/test.controller.ts
@@ -1,36 +1,59 @@
-import dataFile from '../../../TestData.json'
+import dataFile from "../../../TestData.json";
 
+export interface word {
+  id: number;
+  word: string;
+  pos: string;
+}
 class TestController {
-    getWords() {
-        const arr = new Map()
-        
-        dataFile.wordList.forEach((key, index) => {
-           const isExists = Object.keys(arr).includes(key.pos)
-           if(isExists) {
-            arr.get(key.pos).push(index)
-           }
-           else {
-            arr.set(key.pos, [index])
-           }
-        });
+  getWords() {
+    const wordMap = new Map<string, number[]>();
+    const newWordList: word[] = [];
+
+    // Add word pos as unique key in map 
+    dataFile.wordList.forEach((word: word, index: number) => {
+      const isExists = wordMap.get(word.pos);
+
+      if (isExists) {
+        wordMap.get(word.pos).push(index);
+      } else {
+        wordMap.set(word.pos, [index]);
+      }
+    });
+
+    wordMap.forEach((val) => {
+      const rand = Math.floor(Math.random() * val.length);
+      newWordList.push(dataFile.wordList[val[rand]]);
+      delete dataFile.wordList[val[rand]];
+    });
+
+    const questionNumber = 10;
+
+    while (newWordList.length != questionNumber) {
+      const rand = Math.floor(Math.random() * dataFile.wordList.length);
+      newWordList.push(dataFile.wordList[rand]);
+      delete dataFile.wordList[rand];
     }
 
-
-    calculateRank(score) {
-        let points = 0
-        dataFile.scoresList.forEach((s) => {
-            if(s < score) {
-                points++
-            }
-        })
-
-        const rank = (points / dataFile.scoresList.length) * 100
-
-        return {
-            rank: +rank.toFixed(2)
-        }
+    return {
+        words: newWordList
     }
+  }
+
+  calculateRank(score) {
+    let points = 0;
+    dataFile.scoresList.forEach((s) => {
+      if (s < score) {
+        points++;
+      }
+    });
+
+    const rank = (points / dataFile.scoresList.length) * 100;
+
+    return {
+      rank: +rank.toFixed(2),
+    };
+  }
 }
 
-
-export default new TestController()
+export default new TestController();
