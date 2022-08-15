6ec3a8c7f1326fb60b7e772b9b392a94217a8fb8
Mustafa <mustafa.abdelbadea@gmail.com>
2022-08-15 20:26:01 +0200

    Add coloring when stop application

--- twerlo/server/src/boot/servers/index.ts
+++ twerlo/server/src/boot/servers/index.ts
@@ -23,7 +23,7 @@ export async function stop() {
   try {
     if(server) {
       await server.close();
-      console.log('main > boot > server > main > stopped.');
+      console.log(clc.red.bold('main > boot > server > main > stopped.'));
     }
   } catch (error) {
     console.log(error)
